7e9b98de81be2caf9695a09c3b2cbfd71f94c429
Divya Garg <divyagarg2601@gmail.com>
2021-07-08 06:20:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className = "m-5 text-sm">hello</div>
+  )
+}
+
+export default App
