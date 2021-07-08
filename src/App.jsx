994469f3e8b8cf42c74b5bb230b49ad1a6931ecd
Divya Garg <divyagarg2601@gmail.com>
2021-07-08 15:26:28 +0530

    Add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { AppRouter } from "./components/AppRouter";
+import { Loading } from "./components/Loading";
 
 function App() {
   return <AppRouter />;
+  // return <Loading />;
+  // return <div>hello</div>;
 }
 
 export default App;
