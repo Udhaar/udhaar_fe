19314e706a6e561191253a020f80615b8f8280ee
Divya Garg <divyagarg2601@gmail.com>
2021-07-10 13:29:40 +0530

    Complete sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { AppRouter } from "./components/AppRouter";
-import { Loading } from "./components/Loading";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <AppRouter />;
-  // return <Loading />;
-  // return <div>hello</div>;
+  return (
+    <div>
+      <ToastContainer />
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,44 @@
-import React, { useReducer } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const SignUp = () => {
+  const history = useHistory();
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await fetch("http://localhost:8000/api/user/create/", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        const statusCode = response.status;
+        const data = response.json();
+        return Promise.all([statusCode, data]);
+      })
+      .then((res) => {
+        if (res[0] === 201) {
+          toast.success("Registration successful!");
+          history.push("/signin");
+        } else {
+          for (var k in res[1]) {
+            toast.error(res[1][k][0]);
+          }
+        }
+      })
+      .catch((error) => toast.error("Something went wrong"));
+  }
+
   return (
     <div className="min-h-screen bg-[#14213D] text-white">
       {/* Udhaar logo and introduction */}
@@ -13,33 +50,49 @@ export const SignUp = () => {
       {/* signin form */}
       <div className="flex justify-center">
         <div className="bg-[#0164A2] flex flex-col px-8 pt-8 pb-3 rounded-lg">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="md:flex">
               <input
                 type="text"
                 className="block bg-[#14213D] mb-3 text-lg px-4 py-3 rounded-lg border-[3px] border-[#FCA311] w-full md:w-56 mr-2 outline-none"
                 placeholder="First Name"
+                value={formData.first_name}
+                onChange={(e) =>
+                  setFormData({ ...formData, first_name: e.target.value })
+                }
               />
               <input
                 type="text"
                 className="block bg-[#14213D] mb-3 text-lg px-4 py-3 rounded-lg border-[3px] border-[#FCA311] w-full md:w-56 outline-none"
                 placeholder="Last Name"
+                value={formData.last_name}
+                onChange={(e) =>
+                  setFormData({ ...formData, last_name: e.target.value })
+                }
               />
             </div>
             <input
               type="email"
               className="block bg-[#14213D] mb-3 text-lg px-4 py-3 rounded-lg border-[3px] border-[#FCA311] w-full outline-none"
               placeholder="Email Id"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
             />
             <input
               type="password"
               className="block bg-[#14213D] mb-3 text-lg px-4 py-3 rounded-lg border-[3px] border-[#FCA311] w-full outline-none"
               placeholder="Password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
             />
             <input
               type="submit"
               className="block bg-[#14213D] w-full bg-[#FCA311] py-1 text-lg rounded-lg cursor-pointer font-bold"
-              value="Login"
+              value="Sign up"
             />
           </form>
           <p className="text-center text-sm mt-1">
